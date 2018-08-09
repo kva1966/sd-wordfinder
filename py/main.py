4e54156bdc31011041392107fc39d654797fb996
Kamal Advani <kamal@namingcrisis.net>
2018-08-09 12:16:40 +1000

    Better formatting and timing.

--- py/main.py
+++ py/main.py
@@ -1,4 +1,5 @@
 from wordfinder.indexer import Indexer, IndexType
+from time import time
 
 WORD_FILE_PATH = '/usr/share/dict/words'
 
@@ -6,15 +7,24 @@ Indexer.DEBUG = True
 
 if __name__ == '__main__':
   with open(WORD_FILE_PATH) as f:
-    index = Indexer(f, IndexType.LIST).index()
+    index = Indexer(f, IndexType.TREE).index()
 
   print("Index built\n")
 
   try:
     while True:
       q = input("query: ")
+      start_secs = time()
       results = index.query(q, True)
-      print("Results:\n {}\n\n".format(results))
+      query_time_ms = (time() - start_secs) * 1000
+      result_list = list(results)
+      info = """
+Results:
+{}
+{} words found.
+Query took {} ms.     
+""".format(result_list, len(result_list), query_time_ms)
+      print(info)
   except (EOFError, KeyboardInterrupt):
     print("\nBye.")
   except Exception:
